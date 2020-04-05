69bde0b084ee85ff6c0d35248d3e84b4b8d6b23b
francirs <francirs@users.noreply.github.com>
2020-04-05 19:13:24 +0200

    Start

---
+++ starter-code/Java 2.1/js/app.js
@@ -0,0 +1,25 @@
+
+// FUNCTIONS EXERCISE
+
+// 1. Write a function that reverses a string
+function reverseString(string) {
+    var reversedString = "";
+    for(var i = string.length - 1; i >= 0; i--) {
+        reversedString = reversedString + string.charAt(i); 
+    }
+    return reversedString; 
+}
+console.log(reverseString("Hello"));
+
+
+
+// 2. Write a function that sorts a string in alphabetical order
+
+
+// 3. Write a function that loops over an array or Strings and capitalises each one before returning the array 
+
+
+// 4. Write a function that console logs the data type of the aruments
+
+
+// 5. Write a function that returns the length of the longest word in a sentence
